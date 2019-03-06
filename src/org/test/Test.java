1205fcaf301a24a18069227e462c663c03003ed4
shreebhuvana <shreebhuvana6@gmail.com>
2019-03-06 12:24:24 +0530

    new Project

---
+++ src/org/test/Test.java
@@ -0,0 +1,17 @@
+package org.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\user1\\eclipse-workspace_Bhuvana\\Sample\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+	
+	
+	
+	
+	}
+
+}
